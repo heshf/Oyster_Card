a4f70973481ee8556807687fefc227db92a378a0
antoinedoolaeghe <antoinedoolaeghe@gmail.com>
2017-07-11 17:07:26 +0100

    refatcoring

--- lib/station.rb
+++ lib/station.rb
@@ -2,7 +2,7 @@ require_relative 'oyster_card'
 class Station
   attr_reader :zone
   def initialize(zone)
-    fail "zone doesn't exist" if zone != 1
+    fail "zone doesn't exist" if  zone > 7
     @zone = zone
   end
 
--- spec/station_spec.rb
+++ spec/station_spec.rb
@@ -1,13 +1,18 @@
 require 'station'
 
   describe Station do
+    context 'zone1' do
     subject(:station) { described_class.new(zone1) }
     let(:zone1) {1}
-    let(:zone2) {50}
     it 'display the zone the station is in' do
       expect(station.zone).to eq zone1
     end
+  end
+  context 'zone50' do
+    subject(:station1) { described_class.new(zone2) }
+    let(:zone2) {50}
     it "raise an error if zone doesn't exist" do
-      expect { Station.new(zone2) }.to raise_error "zone doesn't exist"
+      expect {station1}.to raise_error "zone doesn't exist"
     end
   end
+end
