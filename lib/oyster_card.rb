3792b39e0517c48b2b3bac0dea07b31f0c54f118
antoinedoolaeghe <antoinedoolaeghe@gmail.com>
2017-07-11 13:34:21 +0100

    step 11 done: can save entry station on touch in and forget on touch out, and refactored

--- lib/oyster_card.rb
+++ lib/oyster_card.rb
@@ -3,12 +3,14 @@ class OysterCard
 	attr_reader :balance
 	MAXIMUM_BALANCE = 90
 	MINIMUM_FARE = 1
-	attr_accessor :in_journey
+	attr_reader :in_journey, :entry_station
 
 
 
 	def initialize
 		@balance = 0
+		@in_journey = false
+		@entry_station = nil
 	end
 
 	def top_up amount
@@ -17,18 +19,20 @@ class OysterCard
 	end
 
 
-	def touch_in
+	def touch_in(location)
 		fail("Seek Assistance: not enough money!") if @balance < MINIMUM_FARE
-		@in_journey = true
+		# @in_journey = true
+		@entry_station = location
 	end
 
 	def touch_out
-		@in_journey = false
+		# @in_journey = false
+		@entry_station = nil
 		deduct(MINIMUM_FARE)
 	end
 
 	def in_journey?
-		@in_journey
+		@entry_station != nil
 	end
 
 	private
--- spec/oyster_card_spec.rb
+++ spec/oyster_card_spec.rb
@@ -4,50 +4,64 @@ describe OysterCard do
 
   subject(:oyster_card) { described_class.new }
   let(:top_up_amount) { 15 }
+  let(:station){ double :station }
   # let(:fare) { 2.80 }
 
-  it "will have a balance " do
-    expect(oyster_card).to respond_to(:balance)
+  describe '#top_up amount' do
+    it 'can top up the balance' do
+      expect{ oyster_card.top_up(top_up_amount) }.to change{ oyster_card.balance }.by top_up_amount
+    end
+
+    it 'cannot have a balance of more than 90' do
+  	  expect { oyster_card.top_up(91) }.to raise_error "Maximum balance exceeded, please keep your balance at £#{OysterCard::MAXIMUM_BALANCE} or below."
+    end
   end
 
-  it 'can be topped up' do
-    expect(oyster_card).to respond_to(:top_up).with(1).argument
+  describe '#touch_in' do
+    before { oyster_card.top_up(top_up_amount) }
+    it 'should store the entry station' do
+      oyster_card.touch_in(station)
+      expect(oyster_card.entry_station).to eq station
+    end
   end
 
-  it 'can top up the balance' do
-    expect{ oyster_card.top_up(top_up_amount) }.to change{ oyster_card.balance }.by top_up_amount
+  describe '#touch_out' do
+    before { oyster_card.top_up(top_up_amount) }
+    it 'should forget the entry station' do
+      oyster_card.touch_out
+      expect(oyster_card.entry_station).to eq nil
+    end
+    it 'should end the journey' do
+      oyster_card.touch_in(station)
+      oyster_card.touch_out
+      expect(oyster_card.in_journey?).to be false
+    end
   end
 
-  it 'cannot have a balance of more than 90' do
-	  expect { oyster_card.top_up(91) }.to raise_error "Maximum balance exceeded, please keep your balance at £#{OysterCard::MAXIMUM_BALANCE} or below."
-  end
 
-  it 'can touch in' do
-  	expect(oyster_card).to respond_to(:touch_in)
-  end
 
-  it 'can touch out' do
-  	expect(oyster_card).to respond_to(:touch_out)
-  end
+  describe '#in_journey?' do
+    it 'should be false by default' do
+      expect(oyster_card.in_journey?).to be false
+    end
 
-  it 'is on a journey' do
-    oyster_card.top_up(top_up_amount)
-  	oyster_card.touch_in
-  	expect(oyster_card).to be_in_journey
+    it 'is on a journey' do
+      oyster_card.top_up(top_up_amount)
+    	oyster_card.touch_in(station)
+    	expect(oyster_card.in_journey?).to be true
+    end
   end
 
   context 'balance is less than £1' do
-    
+
     it 'does not allow touch in if balance is less than £1' do
-      expect{ oyster_card.touch_in }.to raise_error "Seek Assistance: not enough money!"
+      expect{ oyster_card.touch_in(station) }.to raise_error "Seek Assistance: not enough money!"
     end
   end
 
   it 'will have balance deducted when touching out' do
   	oyster_card.top_up(top_up_amount)
-  	oyster_card.touch_in
+  	oyster_card.touch_in(station)
     expect{ oyster_card.touch_out }.to change{ oyster_card.balance }.by -OysterCard::MINIMUM_FARE
   end
-
-
 end
